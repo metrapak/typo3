3bcee3c8ac7d8290af7e1f8066b820af3754c308
metrapak <pokachajloartem@gmail.com>
2026-03-06 09:49:59 +0100

    init

---
+++ packages/my_site_package_1.0.0/ext_emconf.php
@@ -0,0 +1,29 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'My site Package',
+    'description' => '',
+    'category' => 'templates',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '13.4.0-13.4.99',
+            'fluid_styled_content' => '13.4.0-13.4.99',
+            'rte_ckeditor' => '13.4.0-13.4.99',
+        ],
+        'conflicts' => [
+        ],
+    ],
+    'autoload' => [
+        'psr-4' => [
+            'Test\\MySitePackage\\' => 'Classes',
+        ],
+    ],
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 1,
+    'author' => 'Artem',
+    'author_email' => 'test@gmail.com',
+    'author_company' => 'Test',
+    'version' => '1.0.0',
+];
